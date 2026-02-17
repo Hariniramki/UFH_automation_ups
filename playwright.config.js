a6825b14c4973a6887c0597158a4810925a4cf7c
harini.ramakrishnan <harini.ramakrishnan@americold.com>
2026-02-17 16:21:12 +0530

    ch2

--- playwright.config.js
+++ playwright.config.js
@@ -14,12 +14,9 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
-  /* Run tests in files in parallel */
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
 
   /* ✅ Reporters for Jenkins + HTML */
@@ -75,7 +72,6 @@ export default defineConfig({
     // },
   ],
 
-  /* Run your local dev server before starting the tests */
   // webServer: {
   //   command: 'npm run start',
   //   url: 'http://localhost:3000',
